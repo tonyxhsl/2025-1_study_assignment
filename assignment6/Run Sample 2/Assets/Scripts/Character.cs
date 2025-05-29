c9ea0313c4d0ef02d74c4c41bfeaba343a38f44e
tonyxhsl <kimtaehyun258@gmail.com>
2025-05-29 12:13:49 +0900

    assignment6

--- assignment6/Run Sample 2/Assets/Scripts/Character.cs
+++ assignment6/Run Sample 2/Assets/Scripts/Character.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Character : MonoBehaviour
@@ -17,10 +18,11 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // 좌클릭시 RemainJump를 하나 소모하여 CharacterJumpPower의 힘으로 점프한다.
-        // ---------- TODO ---------- 
-        
-        // -------------------- 
+        if (Input.GetKeyDown(KeyCode.Mouse0) && RemainJump > 0)
+        {
+            Jump(CharacterJumpPower);
+            RemainJump--;
+        }
     }
 
     // Jump with power
@@ -31,18 +33,23 @@ public class Character : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        // tag가 Platform인 것과 충돌하면 RemainJump를 초기화한다.
-        // tag가 Obstacle인 것과 충돌하면 게임 오버한다.
-        // ---------- TODO ---------- 
-        
-        // -------------------- 
+        if (col.gameObject.tag == "Platform")
+        {
+            RemainJump = MaxJump;
+        }
+
+        if (col.gameObject.tag == "Obstacle")
+        {
+            GM.GameOver();
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        // tag가 Point인 것과 충돌하면 Point를 하나 얻고, 충돌한 오브젝트를 삭제한다.
-        // ---------- TODO ---------- 
-        
-        // -------------------- 
+        if (col.gameObject.tag == "Point")
+        {
+            GM.GetPoint(1);
+            Destroy(col.gameObject);
+        }
     }
 }
--- assignment6/Run Sample 2/Assets/Scripts/GameManager.cs
+++ assignment6/Run Sample 2/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -34,34 +35,25 @@ public class GameManager : MonoBehaviour
 
     void MoveCam()
     {
-        // CamObj는 Character의 x, y position을 따라간다.
-        // ---------- TODO ---------- 
-        
-        // -------------------- 
+        CamObj.transform.position = new Vector3(Character.transform.position.x, Character.transform.position.y, -10);
     }
 
     void MoveCharacter()
     {
-        // Character는 초당 CharacterSpeed의 속도로 우측으로 움직인다.
-        // ---------- TODO ---------- 
-        
-        // -------------------- 
+        Character.transform.Translate(Vector3.right * CharacterSpeed * Time.deltaTime);
     }
 
     public void GameOver()
     {
-        // Character를 삭제하고, "Game Over!"라는 메시지를 3초간 띄우고, RestartButton을 활성화한다.
-        // ---------- TODO ---------- 
-        
-        // -------------------- 
+        Destroy(Character);
+        MyUIManager.DisplayMessage("Game Over!", 3f);
+        MyUIManager.RestartButton.SetActive(true);
     }
 
     public void GetPoint(int point)
     {
-        // point만큼 점수를 증가시키고 UI에 표시한다.
-        // ---------- TODO ---------- 
-        
-        // -------------------- 
+        NowScore += point;
+        MyUIManager.DisplayScore(NowScore);
     }
 
     // Restart the game
